3a717d4453b008eeba5760674ee4a1bb18963ba5
khoi <6994441+khoi@users.noreply.github.com>
2026-02-04 21:58:05 +0700

    Center command palette in detail pane

--- supacode/App/ContentView.swift
+++ supacode/App/ContentView.swift
@@ -66,12 +66,6 @@ struct ContentView: View {
     .alert(store: repositoriesStore.scope(state: \.$alert, action: \.alert))
     .alert(store: store.scope(state: \.$alert, action: \.alert))
     .focusedSceneValue(\.toggleLeftSidebarAction, toggleLeftSidebar)
-    .overlay {
-      CommandPaletteOverlayView(
-        store: store.scope(state: \.commandPalette, action: \.commandPalette),
-        items: store.commandPaletteItems
-      )
-    }
     .background(WindowTabbingDisabler())
   }
 
